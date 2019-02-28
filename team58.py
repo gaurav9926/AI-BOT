e4e0dea4b85f3bcb6651216aba021f7888a3c6e9
Gaurav Aswani <gaurav.aswani@research.iiit.ac.in>
2019-03-01 04:24:18 +0530

    Updated Files

--- team58.py
+++ team58.py
@@ -3,43 +3,41 @@ import random
 import signal
 import time
 import copy
+from random import randint
 
-class Player48:
+class Player58:
 	def __init__(self):
 		self.infinity = 99999999
 		self.ninfinity = -99999999
 		self.next_move = (0, 0)
 		self.symbol = 'x'
-
+		self.value = [1 , 10 , 100 ]
 
 
 	def evaluation(self, board, old_move):
-		# return random(-1000,1000)
-		return 1
+		# return randint(-1000,1000)
+		# return 1
 
+
+	
+	
 	def move(self, board, old_move, flag):
 		self.symbol = flag
 		utility = self.minimax_search(board, old_move, 0, self.ninfinity, self.infinity, True, flag)
-		return (self.next_move[0], self.next_move[1])
+		return (self.next_move[0], self.next_move[1], self.next_move[2])
 
 	def minimax_search(self, board, old_move, depth, alpha, beta, max_player, flag):
-		#print "Entered"
 		status = board.find_terminal_state();
-		#print status
 		if depth == 3 or status[0] != 'CONTINUE':
-			#return random.randint(0, 1000)
-			#return 1
 			if self.symbol == 'x':
 				return (self.evaluation(board, old_move))
 			else:
 				return (0 - self.evaluation(board, old_move))
 
 		if max_player:
-			#print "Enteredmax"
 			value = self.ninfinity
 			valid_moves = board.find_valid_move_cells(old_move)
 			random.shuffle(valid_moves)
-			#print valid_moves
 			for move in valid_moves:
 				board.update(old_move, move, flag)
 				if flag == 'x':
@@ -48,9 +46,9 @@ class Player48:
 					next_flag = 'x'
 				child_value = self.minimax_search(board, move, depth + 1, alpha, beta, False, next_flag)
 				
-				for k in range(2):		
-					board.big_boards_status[k][move[0]][move[1]] = '-';
-					board.small_boards_status[k][move[0] / 3][move[1] / 3] = '-'
+				board.big_boards_status[move[0]][move[1]][move[2]] = '-';
+				board.small_boards_status[move[0]][move[1] / 3][move[2] / 3] = '-'
+
 				if child_value > value:
 					value = child_value
 					if depth == 0:
@@ -58,10 +56,8 @@ class Player48:
 				alpha = max(alpha, value)				
 				if beta <= alpha:
 					break
-			return value
-
+			# return value
 		else:
-			#print "Enteredmin"
 			value = self.infinity
 			valid_moves = board.find_valid_move_cells(old_move)
 			random.shuffle(valid_moves)
@@ -72,10 +68,10 @@ class Player48:
 				else:
 					next_flag = 'x'
 				child_value = self.minimax_search(board, move, depth + 1, alpha, beta, True, next_flag)
-				#print "Child ended"
-				for k in range(2):	
-					board.big_boards_status[k][move[0]][move[1]] = '-';
-					board.small_boards_status[k][move[0] / 3][move[1] / 3] = '-'
+				
+				board.big_boards_status[move[0]][move[1]][move[2]] = '-';
+				board.small_boards_status[move[0]][move[1] / 3][move[2] / 3] = '-'
+				
 				if child_value < value:
 					value = child_value
 					if depth == 0:
